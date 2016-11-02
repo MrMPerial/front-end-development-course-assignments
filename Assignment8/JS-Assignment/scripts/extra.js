8511e57d558d140c1a416fead62826f6ca6b2b15
Chris <christopher.mitchell26@gmail.com>
2016-11-02 12:30:15 -0500

    Assignment8 - JS

---
+++ Assignment8/JS-Assignment/scripts/extra.js
@@ -0,0 +1,15 @@
+var to = 2;
+var too = 2;
+var two = to + too;
+var firstName = 'Chris';
+var lastName = 'Mitchell';
+var fullName = firstName + lastName;
+var today = Date();
+var pageName = document.title;
+
+console.log(to + too);
+console.log(two);
+document.write(firstName + lastName);
+document.write(fullName);
+document.write(today);
+console.log(pageName);
---
+++ Assignment8/JS-Assignment/scripts/main.js
@@ -0,0 +1,11 @@
+var day;
+var cups;
+var green;
+
+day = "Wednesday";
+cups = 1;
+green = false;
+
+console.log(day);
+console.log(cups);
+console.log(green);
