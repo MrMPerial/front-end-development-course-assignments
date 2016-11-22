95f3822689a501dcfc3e5ba0f43c0143df3956cd
Chris <christopher.mitchell26@gmail.com>
2016-11-22 15:45:49 -0600

    Code Cleanup

--- Assessment2/scripts/main.js
+++ Assessment2/scripts/main.js
@@ -1,11 +1,15 @@
+// Button
 document.getElementById('btnSubmit').addEventListener('click', submit);
 
+// Submit Function
 function submit() {
 
+    // Variables
     var error = [];
     var errorClass = 'invalid';
     var input = ['firstName', 'lastName', 'email', 'explain'];
 
+    // Check For Empty Space
     for ( var index = 0; index < input.length; index++ ) {
         var element = document.getElementById(input[index]);
         if ( element.value === '' ) {
@@ -16,34 +20,7 @@ function submit() {
         }
     }
 
-    /*if ( firstName.value === '' ) {
-        error.push(firstName.placeholder + ' Is Required');
-        firstName.classList.add(errorClass);
-    } else {
-        firstName.classList.remove(errorClass);
-    }
-
-    if ( lastName.value === '' ) {
-        error.push(lastName.placeholder + ' Is Required');
-        lastName.classList.add(errorClass);
-    } else {
-        lastName.classList.remove(errorClass);
-    }
-
-    if ( email.value === '' ) {
-        error.push(email.placeholder + ' Is Required');
-        email.classList.add(errorClass);
-    } else {
-        email.classList.remove(errorClass);
-    }
-
-    if ( explain.value === '' ) {
-        error.push('Why You Need This Is Required');
-        explain.classList.add(errorClass);
-    } else {
-        explain.classList.remove(errorClass);
-    }*/
-
+    // Validate Email
     if ( email.value.indexOf('@') < 0 || email.value.indexOf('.com') < 0 ) {
         error.push("A Valid Email is Required");
         email.classList.add(errorClass);
@@ -51,6 +28,7 @@ function submit() {
         email.classList.remove(errorClass);
     }
 
+    // Validate Options
     if ( hearAbout.value < 1 ) {
         error.push('How You Found Us Is Required');
         hearAbout.classList.add(errorClass);
@@ -58,6 +36,7 @@ function submit() {
         hearAbout.classList.remove(errorClass);
     }
 
+    // Validate Terms
     if ( terms.checked === false ) {
         error.push("You Must Accept The Terms To Submit");
         terms.classList.add(errorClass);
@@ -65,6 +44,7 @@ function submit() {
         terms.classList.remove(errorClass);
     }
 
+    // Display Error Message
     var html;
 
     if ( error.length > 0 ) {
@@ -73,6 +53,7 @@ function submit() {
         html = '<h1 class="thankYou">Thank you for your submission!</h1>';
     }
 
+    // Post to Document
     document.getElementById('errorMessages').innerHTML = html;
 
 }
