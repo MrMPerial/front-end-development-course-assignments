29cbf6146f54813410b7553a898ad4d2af4e8510
Chris <christopher.mitchell26@gmail.com>
2016-11-22 14:20:00 -0600

    Updated Error Classes

--- Assessment2/scripts/main.js
+++ Assessment2/scripts/main.js
@@ -14,26 +14,44 @@ function submit() {
 
     if ( lastName.value === '' ) {
         error.push(lastName.placeholder + ' Is Required');
+        lastName.classList.add(errorClass);
+    } else {
+        lastName.classList.remove(errorClass);
     }
 
     if ( email.value === '' ) {
         error.push(email.placeholder + ' Is Required');
+        email.classList.add(errorClass);
+    } else {
+        email.classList.remove(errorClass);
     }
 
     if ( explain.value === '' ) {
         error.push('Why You Need This Is Required');
+        explain.classList.add(errorClass);
+    } else {
+        explain.classList.remove(errorClass);
     }
 
     if ( email.value.indexOf('@') < 0 || email.value.indexOf('.com') < 0 ) {
         error.push("A Valid Email is Required");
+        email.classList.add(errorClass);
+    } else {
+        email.classList.remove(errorClass);
     }
 
     if ( hearAbout.value < 1 ) {
         error.push('How You Found Us Is Required');
+        hearAbout.classList.add(errorClass);
+    } else {
+        hearAbout.classList.remove(errorClass);
     }
 
     if ( terms.checked === false ) {
         error.push("You Must Accept The Terms To Submit");
+        terms.classList.add(errorClass);
+    } else {
+        terms.classList.remove(errorClass);
     }
 
     var html;
@@ -41,7 +59,7 @@ function submit() {
     if ( error.length > 0 ) {
         html = '<ul><li>' + error.join('</li><li>') + '</li></ul>';
     } else {
-        html = '<h1>Thank you for your submission!</h1>';
+        html = '<h1 class="thankYou">Thank you for your submission!</h1>';
     }
 
     document.getElementById('errorMessages').innerHTML = html;
