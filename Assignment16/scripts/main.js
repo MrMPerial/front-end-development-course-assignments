6a2f7496441e51364ae047cbc7b9758d36af6da7
Chris <christopher.mitchell26@gmail.com>
2016-12-01 18:37:43 -0600

    Assignment16

--- Assignment16/scripts/main.js
+++ Assignment16/scripts/main.js
@@ -1,5 +1,54 @@
 $(document).ready(function(){
 
-
+    // Part-2
+    
+    // Show
+    $('#btnShow').on('click', function() {
+        $('.display-basic').show();
+    });
+    
+    // Hide
+    $('#btnHide').on('click', function() {
+        $('.display-basic').hide();
+    });
+    
+    // Toggle
+    $('#btnToggle').on('click', function() {
+        $('.display-basic').toggle();
+    });
+    
+    // Part-3
+    
+    // Fade In
+    $('#btnFadeIn').on('click', function() {
+        $('.display-fade').fadeIn();
+    });
+    
+    // Fade Out
+    $('#btnFadeOut').on('click', function() {
+        $('.display-fade').fadeOut();
+    });
+    
+    // Fade Toggle
+    $('#btnFadeToggle').on('click', function() {
+        $('.display-fade').fadeToggle();
+    });
+    
+    // part-4
+    
+    // Slide Down
+    $('#btnSlideDown').on('click', function() {
+        $('.display-slide').slideDown(1000);
+    });
+    
+    // Slide Up
+    $('#btnSlideUp').on('click', function() {
+        $('.display-slide').slideUp(500);
+    });
+    
+    // Slide Toggle
+    $('#btnSlideToggle').on('click', function() {
+        $('.display-slide').slideToggle();
+    });
 
 });
