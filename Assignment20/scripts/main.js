0f91915fff6e66648a3f58a3e1354557f1206f75
Christopher Mitchell <mrmperial@MrMPerial.local>
2016-12-16 16:42:05 -0600

    Assignment 20 - Form Validation

--- Assignment20/scripts/main.js
+++ Assignment20/scripts/main.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
 
+    var formName = document.getElementById('name');
+
+    $('input').on('blur', function () {            
+        if (formName.value === '') {                
+            $(this).addClass('errors');
+            $('.errMsg1').show();
+        } else {
+            $(this).removeClass('errors');
+            $('.errMsg1').hide();
+        }
+    });
+
+    $('#subBtn').on('click', function () {
+        if (formName.value === '') {
+            $('.errMsg1').show();
+        } else {
+            $('.errMsg1').hide();
+            $('#formAnswer').slideUp();
+            $('#formThanks').slideDown();
+        }
+    });
+
+    $('#addAnswer').on('click', function () {
+        $('#formThanks').slideUp();
+        $('#formAnswer').slideDown();
+    });
+    
+    //
 
 });
