2c67b03c5a835915111d9fd2c3afb0edb0e88bdd
Christopher Mitchell <mrmperial@MrMPerial.local>
2017-01-20 11:52:17 -0600

    Assignment 25 - Added Dropdown to desktop menu

--- Assignment25/main.js
+++ Assignment25/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
+    var dropBefore = document.getElementById('dropBtn');
+    var dropAfter = document.getElementById('dropdown');
+
+    $(dropAfter).hide();
+
+    $(dropBefore).on('click', function () {
+        $(dropAfter).slideToggle();
+    });
+
 
 
 });
